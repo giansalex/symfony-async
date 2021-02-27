9ab3ad4208f8c81a2e048360cb2cec0cd8800f22
Giancarlos Salas <giansalex@gmail.com>
2021-02-27 10:56:42 -0500

    Add async server

---
+++ src/AppKernelAdapter.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use Drift\HttpKernel\AsyncKernel;
+use App\Kernel as ApplicationKernel;
+use Drift\Server\Adapter\DriftKernel\SyncKernelException;
+use Drift\Server\Adapter\SymfonyKernelBasedAdapter;
+use Exception;
+use React\Promise\Promise;
+use React\Promise\PromiseInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Kernel;
+
+/**
+ * Class AppKernelAdapter.
+ */
+class AppKernelAdapter extends SymfonyKernelBasedAdapter
+{
+    /**
+     * @param $kernel
+     *
+     * @throws Exception
+     */
+    protected function checkKernel($kernel)
+    {
+        if (!$kernel instanceof AsyncKernel) {
+            throw new SyncKernelException('The kernel should implement AsyncKernel interface, as you are using the DriftPHP kernel adapter');
+        }
+    }
+
+    /**
+     * @param string $environment
+     * @param bool   $debug
+     *
+     * @return Kernel
+     */
+    protected static function createKernelByEnvironmentAndDebug(
+        string $environment,
+        bool $debug
+    ): Kernel {
+        return new ApplicationKernel($environment, $debug);
+    }
+
+    /**
+     * @return PromiseInterface
+     */
+    protected function preload(): PromiseInterface
+    {
+        return $this
+            ->kernel
+            ->preload();
+    }
+
+    /**
+     * @param Kernel $kernel
+     * @param Request $request
+     *
+     * @return PromiseInterface
+     */
+    protected function kernelHandle(
+        Kernel $kernel,
+        Request $request
+    ): PromiseInterface {
+        return $kernel->handleAsync($request);
+    }
+
+    /**
+     * @return PromiseInterface
+     */
+    public function shutDown(): PromiseInterface
+    {
+        $resolver = function (callable $resolve, callable $reject) {
+           $this->kernel->shutdown();
+           $resolve();
+        };
+
+        return new Promise($resolver);
+    }
+
+    /**
+     * Get watcher folders.
+     *
+     * @return string[]
+     */
+    public static function getObservableFolders(): array
+    {
+        return ['src', 'public', 'views'];
+    }
+}
