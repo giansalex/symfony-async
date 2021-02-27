3b18eaed2b816de1976120512b0ef29d87fc4496
Giancarlos Salas <giansalex@gmail.com>
2021-02-27 10:56:18 -0500

    Configure async kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,9 +2,9 @@
 
 namespace App;
 
+use Drift\HttpKernel\AsyncKernel as BaseKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
